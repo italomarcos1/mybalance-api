d71396dd92728c0a8d051d692b8c0e3e56d9ece6
Italo M <italomarcos0010@gmail.com>
2021-10-08 19:09:23 -0300

    update avatar to support a longer string

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -10,7 +10,7 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.string('avatar', 72)
+      table.string('avatar', 86)
       table.timestamps()
     })
   }
